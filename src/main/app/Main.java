95c4416b3e93390ceaeb991f99b0540a3dfcc275
cristiczika <cristianczika6@gmail.com>
2025-10-20 18:31:16 +0300

    Main a sd

--- src/main/app/Main.java
+++ src/main/app/Main.java
@@ -1,9 +1,19 @@
 package main.app;
 
+import main.tasks.ShopUtils;
+
 public class Main {
 
     static void main() {
-
+        IO.println("------- Aufgabe 4 --------");
+        int[] keyboards1 = {40, 35, 70, 15, 45};
+        IO.println(ShopUtils.getCheapestKeyboard(keyboards1));
+        int[] keyboards2 = {15, 20, 10, 35}, usbs = {20, 15, 40, 15};
+        IO.println(ShopUtils.getMostExpensiveItem(keyboards2, usbs));
+        int[] preise = {15, 45, 20};
+        IO.println(ShopUtils.getMostExpensiveUsbWithinBudget(preise,30));
+        int[] tastaturen = {40, 50, 60}, usb_laufwerke = {8, 12};
+        IO.println(ShopUtils.getMaxSpend(tastaturen, usb_laufwerke, 60));
     }
 
 }
