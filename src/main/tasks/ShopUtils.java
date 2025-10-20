759c08f76aa16c97180c66b906682eda3065e3b7
cristiczika <cristianczika6@gmail.com>
2025-10-20 18:29:03 +0300

    1)

--- src/main/tasks/ShopUtils.java
+++ src/main/tasks/ShopUtils.java
@@ -1,4 +1,17 @@
 package main.tasks;
 
 public class ShopUtils {
+
+    public static int getCheapestKeyboard(int[] keyboards) {
+        int min = keyboards[0];
+
+        for (int i = 0; i < keyboards.length; i++) {
+            if (keyboards[i] < min) {
+                min = keyboards[i];
+            }
+        }
+
+        return min;
+    }
+
 }
